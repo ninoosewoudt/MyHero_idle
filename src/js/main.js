6d7173444b34f41f3f5c9cd5e26e28e1662b8875
ninoosewoudt <nino.osewoudt@gmail.com>
2017-10-13 16:26:10 +0200

    started main file implementing the basics

---
+++ src/js/main.js
@@ -0,0 +1,71 @@
+let game = {};
+
+game.baseInterval = 1000;
+game.player_name= "test";
+
+
+game.resources = {
+  iron : {
+    label : "Stone",
+    currentVal : 0
+  }
+};
+game.skills = {
+  health : {
+    label : "Health",
+    currentVal : 1,
+    xpNeeded : 100,
+    currentXp : 0
+  },
+  attack : {
+    label : "Attack",
+    currentVal : 1,
+    xpNeeded : 100,
+    currentXp : 0
+  },
+  strength : {
+    label : "strength",
+    currentVal : 1,
+    xpNeeded : 100,
+    currentXp : 0
+  },
+  defense : {
+    label : "Defense",
+    currentVal : 1,
+    xpNeeded : 100,
+    currentXp : 0
+  },
+  range : {
+    label : "Range",
+    currentVal : 1,
+    xpNeeded : 100,
+    currentXp : 0
+  },
+  magic : {
+    label : "Magic",
+    currentVal : 1,
+    xpNeeded : 100,
+    currentXp : 0
+  }
+};
+game.currency = {
+  goldenCoins : {
+    label : "Golden Coins",
+    currentVal : 0
+  },
+  silverCoins : {
+    label : "Silver Coins",
+    currentVal : 0
+  }
+};
+game.buildings = {};
+
+
+$(document).ready(function(){
+
+
+
+
+
+
+});
